12cb82a22b8b910927aa03f9972da3cf61b98771
Rajin Sasi <sasi.rajinkumar@gmail.com>
2016-10-24 08:29:43 +0530

    10-24-2016 class

---
+++ js/all.js
@@ -0,0 +1,182 @@
+/*var x = 10;
+console.log(x);
+function test(){
+	var x = 5 ;
+}
+console.log(x);
+test();*/
+
+
+/*function test1(){
+	
+}
+console.log(x);
+test1();*/
+
+/*function test4(){
+	document.getElementById("demo2").innerHTML = document.getElementById("demo1").innerHTML;
+	 document.getElementById("demo1").innerHTML= "";
+}*/
+/*var x = 10;
+var y = "hai rakesh";
+var z = x + y;
+var w = x-y;
+var u = x*y;
+var v = y%x;
+var a = ++(x);
+var b = --(y);
+var c = {name:"brahmam",age:28,color:"blak",status:"good"};
+var d = null;
+var e;
+
+lastIndexOf();
+toUpperCase();
+
+document.write(y);
+console.log(z);
+console.log(w);
+console.log(u);
+console.log(v);
+console.log(a);
+console.log(b);
+console.log(z);
+console.log(c.status);
+console.log(x.length);*/
+
+
+
+//////////////////////////////////
+/*function changeColor(){
+	var x= document.getElementById("#test1").innerHTML="second text";
+	
+};
+
+
+var x = 10;
+if (x < 6){
+	console.log("correct");
+}
+else{
+	console.log("wrong");
+}*/
+
+//document.getElementById("test_demo").style = "background-color:red;color:#ffffff;width:300px;height:300px;";
+//document.getElementById("test_demo").setAttribute("style" , "background-color:red;color:#ffffff;width:300px;height:300px;");
+// var x = 10;
+// var y = "first";
+// var myFirst = [0 , 1 , "brahmam"];
+// var myFirstOjec = {"name":"brahmam" , "age":"28"};
+// var changeType = x.toString();
+// console.log(x);
+// console.log(y);
+// console.log(myFirst[2]="brahmam1");
+// console.log(myFirstOjec.name = "brahmam2");
+// console.log(typeof(myFirst));
+// console.log(x.toString());
+// console.log(typeof(changeType));
+// var w  = 10;
+// var x= 5;
+// var add = w + x;
+// console.log(add);
+// var x = 10;
+// function test() {
+// 	var y ="first";
+// 	console.log(y);
+// };
+// test();
+// console.log(x);
+// var x = 3;
+// var y = "brahmam";
+// var z= 4;
+// var w = (x+y+z);
+// console.log(w);
+// //Result:5 3brahmam4
+// var x = 6;
+// undefined
+// var y=10;
+// var w = "brahmam";
+// console.log(x+y+w);
+// VM334:3 16brahmam
+// undefined
+// console.log(w+x+y);
+// VM359:1 brahmam610
+// var y = 4;
+// function test() {
+// 	var x = 3;
+// 	var y = 6;
+	
+// };
+// test();
+// console.log(y);
+// function demo() {
+// 	 document.getElementById("second").innerHTML = document.getElementById("first").innerHTML;
+// 	 document.getElementById("first").innerHTML = "";
+// }
+// function demo() {
+// 	document.getElementById("first").style = "width:300px;height:300px;background-color:#000000;color:#ffffff;";
+// 	document.getElementById("second").setAttribute({width:300px;height:300px;background-color:#cccccc;color:red;});
+// }
+// var x = 10;
+// function test() {
+// 	console.log(x);
+// 	console.log(name);
+// 	var name = "brahmam";
+// 	console.log(test1());
+// 	console.log(test2);
+// 	function test1() {
+// 		var x =  6 ;
+// 		function test2() {
+// 			var x =  10 ;
+// 			var y = 6 ;
+// 			var z = (x+y);
+// 			console.log(z);
+// 		}
+// 	}
+
+// }
+// test();
+
+document.getElementById("second").setAttribute("style" , "background-color:red;width:300px;height:300px;");
+document.getElementById("first").setAttribute("href" , "https://www.google.co.in");
+var x = [1 ,2,3,"first" ];
+console.log(x[2]=4);
+//string method
+concat()
+
+var name = "brahmam";
+var name1 = "farook";
+var z = name.concat(name1);
+
+var x = 10;
+var y = 11;
+var z = "name1"
+var w = x + y + z;
+console.log(w);
+---------------------
+toUpperCase()
+---------------------
+var name = "brahmaiah";
+var y = name.toUpperCase();
+console.log(y);
+VM545:3 BRAHMAIAH
+-----------------------
+length
+-----------------------
+var name = "brahmaiah";
+var y = name.length;
+console.log(y);
+----------------------
+toLowerCase()
+----------------------
+var name = "BRAHMAIAH";
+var y = name.toLowerCase();
+console.log(y);
+ brahmaiah
+---------------------
+slice()
+---------------------
+var x = [1,2,3,4,5,6];
+var y = x.slice(2,3);
+console.log(y);
+ [3]
+ --------------------
